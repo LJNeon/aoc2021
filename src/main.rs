1d17595056dd9c86d2bf83b8d2c900f304bcc5d0
LJNeon <23249107+LJNeon@users.noreply.github.com>
2022-05-05 21:25:16 -0700

    Day 5A & 5B

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ fn main() {
   println!("| {0:^8} | {1:<10} | {2:<10} |", "Day 2", day2::solve_a(), day2::solve_b());
   println!("| {0:^8} | {1:<10} | {2:<10} |", "Day 3", day3::solve_a(), day3::solve_b());
   println!("| {0:^8} | {1:<10} | {2:<10} |", "Day 4", day4::solve_a(), day4::solve_b());
+  println!("| {0:^8} | {1:<10} | {2:<10} |", "Day 5", day5::solve_a(), day5::solve_b());
   println!("|----------|------------|------------|");
 }
